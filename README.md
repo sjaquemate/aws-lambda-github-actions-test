e6ee695c9fe1b7043647fa1f3401e4d83ab04a8b
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-09 20:28:23 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aws-lambda-github-actions-test
+
+https://www.youtube.com/watch?v=UQiRhKgQ5X0
---
+++ lambda_function.py
@@ -0,0 +1,14 @@
+import json
+import random
+
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(random.choice(['foo bar', 'bar foo']))
+    }
