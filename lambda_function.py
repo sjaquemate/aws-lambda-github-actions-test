c8e9c5e94fd45300fb0e40de02cabd558a716e7e
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-09 21:03:54 +0200

    logger

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,20 @@
-import json
-import random
+import os
+import logging
+import jsonpickle
+import boto3
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+patch_all()
+
+client = boto3.client('lambda')
+client.get_account_settings()
 
 def lambda_handler(event, context):
-    
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        'body': json.dumps(context)
-    }
+    logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
+    logger.info('## EVENT\r' + jsonpickle.encode(event))
+    logger.info('## CONTEXT\r' + jsonpickle.encode(context))
+    response = client.get_account_settings()
+    return response['AccountUsage']
