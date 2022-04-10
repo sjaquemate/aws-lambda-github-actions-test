6b91780c915abd4b00b2e5d8a65368ba336d0918
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-10 12:47:11 +0200

    test decorator

--- lambda_function.py
+++ lambda_function.py
@@ -1,41 +1,7 @@
-import json 
+[ERROR] Runtime.ImportModuleError: Unable to import module 'lambda_function': No module named 'aws_lambda_decorators'
+from aws_lambda_decorators import cors 
 
 
-def do_something(dictionary: dict) -> dict:
-    """ echos post body """
-    return dictionary 
-
-
+@cors(allow_origin='*', allow_methods='POST', allow_headers='Content-Type', max_age=86400)
 def lambda_handler(event, context):
-    try:
-        print('Event: {}'.format(event))
-        print("Log stream name:", context.log_stream_name)
-        print("Log group name:", context.log_group_name)
-        print("Request ID:", context.aws_request_id)
-        print("Mem. limits(MB):", context.memory_limit_in_mb)
-    except Exception as e:  
-        raise Exception('Error occurred during execution')  # notify aws of failure
-    
-    # catch preflight response. Messy, but don't know how to fix... using httpstatus?
-    try:
-        body = event['body'] 
-    except Exception as e: 
-        return {
-            'statusCode': 204,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-            }
-        } 
-    
-    response_body = do_something(json.loads(body))
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        'body': response_body
-    }
+    return {'statusCode': 200, 'body': 'returns something here'}
