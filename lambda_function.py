0e1dfd48beccd7e7a296ff873a844da24fa626d5
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-10 13:06:38 +0200

    make http request

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,24 @@
-[ERROR] Runtime.ImportModuleError: Unable to import module 'lambda_function': No module named 'aws_lambda_decorators'
-from aws_lambda_decorators import cors 
+# using httprequest from source:
+# https://github.com/alfonsof/aws-python-examples/blob/master/awslambdahttprequest/lambda_function.py
 
 
-@cors(allow_origin='*', allow_methods='POST', allow_headers='Content-Type', max_age=86400)
+import json
+
 def lambda_handler(event, context):
-    return {'statusCode': 200, 'body': 'returns something here'}
+    
+    if 'queryStringParameters' in event:
+        body = {'first_name': event['queryStringParameters']['first_name'], 
+                'last name': event['queryStringParameters']['last_name']}
+    else:
+        body = {}
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps(body),
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        }
+    }
+    
